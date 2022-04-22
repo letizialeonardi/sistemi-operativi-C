7d24896d35c4604afaaaaf8a4948de2559ac0a68
letizialeonardi <73306330+letizialeonardi@users.noreply.github.com>
2022-04-22 19:43:12 +0200

    Update myhead1.c
    Ho inserito direttamente le correzioni nel suo codice.
    Buon fine settimana
    
    L. Leonardi

--- esercizi/c/es8/myhead1.c
+++ esercizi/c/es8/myhead1.c
@@ -10,7 +10,8 @@
 int main(int argc, char **argv){
 	
 	char buffer;
-	int nread = 1;
+	/* int nread = 1; non usata */
+	int n; /* variabile aggiunta */
 	
 	if(argc != 2){
 		printf("Devo inserire esattamente un'opzione\n");
@@ -21,9 +22,23 @@ int main(int argc, char **argv){
 		printf("Devo inserire esattamente il parametro -n\n");
 		exit(2);
 	}
-	
-	int i = 0;
-	while(i < atoi(&argv[1][1])){
+	/* dopo avere verificato che il parametro inizi per -, si deve fare la conversione e controllare che il risultato sia strettamente positivo! */
+	n = atoi(&argv[1][1]);
+	if (n <= 0)  
+        {
+                printf("Errore: l'opzione non e' corretta\n");
+		exit (3);
+	}
+	int i = 0; 
+	/* In generale e' sempre meglio fare un while esterno con la lettura */
+	while (	read(0, &buffer, 1) )
+	{	
+		/* controlliamo se il carattere e' il terminatore di linea */
+		if(buffer == '\n') i++; /* incrementiamo il contatore (a me sembrerebbe piu' chiaro inizializzarlo a 1, ma non ho voluto cambiare troppo la sua soluzione ...*/
+		write(1, &buffer, 1); /* scriviamo comunque il carattere sia che sia \n o un altro carattere */
+		/* ora dobbiamo controllare se dobbiamo uscire */
+		if (i == n) break;
+	/* while(i < atoi(&argv[1][1])){
 		while(buffer != '\n'){	
 			nread = read(0, &buffer, 1);
 			if(buffer == '\n'){
@@ -34,7 +49,8 @@ int main(int argc, char **argv){
 			}
 		}
 		i+=1;
-		buffer = ' ';
+		buffer = ' '; nota bene: questo reset direi che in generale non sia mai necessario ... 
+		*/
 	}
 	
 	exit(0);	
